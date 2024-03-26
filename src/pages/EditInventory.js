78d1ceca8895e274c24aad29d17de1184a9ea9b5
mariam1798 <Mariam.alneamah@gmail.com>
2024-03-26 16:38:58 +0000

    add header to edit inventory

--- src/pages/EditInventory.js
+++ src/pages/EditInventory.js
@@ -2,9 +2,19 @@ import PageHeader from "../components/PageHeader/PageHeader";
 import InventoryForm from "../components/InventoryForm/InventoryForm";
 
 function EditInventory() {
+	const headerConfig = {
+		backButton: {
+			show: true,
+			path: "/inventory",
+		},
+		searchBar: false,
+		actionButton: {
+			show: false,
+		},
+	};
 	return (
 		<>
-			<PageHeader />
+			<PageHeader title="Edit Inventory" config={headerConfig} />
 			<InventoryForm />
 		</>
 	);
