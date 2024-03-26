4ea600c2ed381ea74eb6f92208146a27524ecf4f
tamaraakguneyli <tamara.akguneyli@hotmail.co.uk>
2024-03-26 15:22:25 +0000

    styling for warehouse form

--- src/components/WarehouseForm/WarehouseForm.js
+++ src/components/WarehouseForm/WarehouseForm.js
@@ -1,42 +1,82 @@
 import React from "react";
+import "../WarehouseForm/WarehouseForm.scss";
 
 function WarehouseForm() {
   return (
-    <form>
-      <div className="forms">
-        <div className="form">
-          <h1>Warehouse Details</h1>
-          <label>Warehouse Name</label>
+    <div className="forms">
+      <form>
+        <div className="forms__form">
+          <h1 className="forms__header">Warehouse Details</h1>
+          <label className="forms__label">Warehouse Name</label>
           <input
             type="text"
             id="warehouse-name"
-            className=""
+            className="forms__input"
             placeholder="API"
           />
-          <label>Street Address</label>
-          <input type="text" id="address" className="" placeholder="API" />
-          <label>City</label>
-          <input type="text" id="city" className="" placeholder="API" />
-          <label>Country</label>
-          <input type="text" id="country" className="" placeholder="API" />
+          <label className="forms__label">Street Address</label>
+          <input
+            type="text"
+            id="address"
+            className="forms__input"
+            placeholder="API"
+          />
+          <label className="forms__label">City</label>
+          <input
+            type="text"
+            id="city"
+            className="forms__input"
+            placeholder="API"
+          />
+          <label className="forms__label">Country</label>
+          <input
+            type="text"
+            id="country"
+            className="forms__input"
+            placeholder="API"
+          />
         </div>
-        <div className="form">
-          <h1>Warehouse Details</h1>
-          <label>Contact Name</label>
-          <input type="text" id="contact-name" className="" placeholder="API" />
+        <div className="forms__form">
+          <h1 className="forms__header">Contact Details</h1>
+          <label className="forms__label">Contact Name</label>
+          <input
+            type="text"
+            id="contact-name"
+            className="forms__input"
+            placeholder="API"
+          />
           <label>Position</label>
-          <input type="text" id="position" className="" placeholder="API" />
-          <label>Phone Number</label>
-          <input type="text" id="number" className="" placeholder="API" />
-          <label>Email</label>
-          <input type="text" id="email" className="" placeholder="API" />
+          <input
+            type="text"
+            id="position"
+            className="forms__input"
+            placeholder="API"
+          />
+          <label className="forms__label">Phone Number</label>
+          <input
+            type="text"
+            id="number"
+            className="forms__input"
+            placeholder="API"
+          />
+          <label className="forms__label">Email</label>
+          <input
+            type="text"
+            id="email"
+            className="forms__input"
+            placeholder="API"
+          />
         </div>
-      </div>
-      <div>
-        <button type="submit">Cancel</button>
-        <button type="submit">Save</button>
-      </div>
-    </form>
+        <div className="forms__buttons">
+          <button type="submit" className="forms__button forms__button--cancel">
+            Cancel
+          </button>
+          <button type="submit" className="forms__button forms__button--save">
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
