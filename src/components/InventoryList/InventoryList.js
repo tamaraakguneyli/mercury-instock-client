ee0bdd6b01907f1375747c37053f11db1c658572
Vladimir Cascarade <vladimir.cascarade-ext@kering.com>
2024-03-26 16:15:04 +0000

    Remove unwanted upstream changes

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -1,36 +1,7 @@
 import React from "react";
-import "./InventoryList.scss";
-import { Link } from "react-router-dom";
 
 function InventoryList() {
-  const inStock = true;
-  const id = 1;
-  return (
-    <div className="list">
-      <div className="list__left">
-        <h4 className="list__title">inventory item</h4>
-        <Link to={`/inventory/${id}`} className="list__data list__link">
-          Television
-        </Link>
-        <h4 className="list__title">category</h4>
-        <p className="list__data list__category">Electronics</p>
-      </div>
-      <div className="list__right">
-        <h4 className="list__title">status</h4>
-        <p
-          className={`list__data list__status ${
-            inStock ? "" : "list__status--out"
-          }`}
-        >
-          In stock
-        </p>
-        <h4 className="list__title">qty</h4>
-        <p className="list__data list__qty">500</p>
-        <h4 className="list__title">warehouse</h4>
-        <p className="list__data list__warehouse">Manhattan</p>
-      </div>
-    </div>
-  );
+  return <div>InventoryList</div>;
 }
 
 export default InventoryList;
