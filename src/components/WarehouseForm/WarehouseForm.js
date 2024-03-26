24800f11e0a8f68d707b55559327093761a8b708
tamaraakguneyli <tamara.akguneyli@hotmail.co.uk>
2024-03-26 20:05:54 +0000

    add global styling form WarehouseForm

--- src/components/WarehouseForm/WarehouseForm.js
+++ src/components/WarehouseForm/WarehouseForm.js
@@ -3,80 +3,94 @@ import "../WarehouseForm/WarehouseForm.scss";
 
 function WarehouseForm() {
   return (
-    <div className="forms">
-      <form>
-        <div className="forms__form">
-          <h1 className="forms__header">Warehouse Details</h1>
-          <label className="forms__label">Warehouse Name</label>
-          <input
-            type="text"
-            id="warehouse-name"
-            className="forms__input"
-            placeholder="API"
-          />
-          <label className="forms__label">Street Address</label>
-          <input
-            type="text"
-            id="address"
-            className="forms__input"
-            placeholder="API"
-          />
-          <label className="forms__label">City</label>
-          <input
-            type="text"
-            id="city"
-            className="forms__input"
-            placeholder="API"
-          />
-          <label className="forms__label">Country</label>
-          <input
-            type="text"
-            id="country"
-            className="forms__input"
-            placeholder="API"
-          />
-        </div>
-        <div className="forms__form">
-          <h1 className="forms__header">Contact Details</h1>
-          <label className="forms__label">Contact Name</label>
-          <input
-            type="text"
-            id="contact-name"
-            className="forms__input"
-            placeholder="API"
-          />
-          <label>Position</label>
-          <input
-            type="text"
-            id="position"
-            className="forms__input"
-            placeholder="API"
-          />
-          <label className="forms__label">Phone Number</label>
-          <input
-            type="text"
-            id="number"
-            className="forms__input"
-            placeholder="API"
-          />
-          <label className="forms__label">Email</label>
-          <input
-            type="text"
-            id="email"
-            className="forms__input"
-            placeholder="API"
-          />
-        </div>
-        <div className="forms__buttons">
-          <button type="submit" className="forms__button forms__button--cancel">
-            Cancel
-          </button>
-          <button type="submit" className="forms__button forms__button--save">
-            Save
-          </button>
-        </div>
-      </form>
-    </div>
+    <main className="page">
+      <article className="page__content">
+        <form>
+          <div className="layout">
+            <div className="page__top-divider">
+              <div className="layout__block layout__block--middle-border">
+                <h1 className="layout__headers">Warehouse Details</h1>
+                <label className="layout__form-labels">Warehouse Name</label>
+                <input
+                  type="text"
+                  id="warehouse-name"
+                  className="layout__form-inputs"
+                  placeholder="API"
+                />
+                <label className="layout__form-labels">Street Address</label>
+                <input
+                  type="text"
+                  id="address"
+                  className="layout__form-inputs"
+                  placeholder="API"
+                />
+                <label className="layout__form-labels">City</label>
+                <input
+                  type="text"
+                  id="city"
+                  className="layout__form-inputs"
+                  placeholder="API"
+                />
+                <label className="layout__form-labels">Country</label>
+                <input
+                  type="text"
+                  id="country"
+                  className="layout__form-inputs"
+                  placeholder="API"
+                />
+              </div>
+            </div>
+            <div className="page__top-divider">
+              <div className="layout__block">
+                <h1 className="layout__headers">Contact Details</h1>
+                <label className="layout__form-labels">Contact Name</label>
+                <input
+                  type="text"
+                  id="contact-name"
+                  className="layout__form-inputs"
+                  placeholder="API"
+                />
+                <label className="layout__form-labels">Position</label>
+                <input
+                  type="text"
+                  id="position"
+                  className="layout__form-inputs"
+                  placeholder="API"
+                />
+                <label className="layout__form-labels">Phone Number</label>
+                <input
+                  type="text"
+                  id="number"
+                  className="layout__form-inputs"
+                  placeholder="API"
+                />
+                <label className="layout__form-labels">Email</label>
+                <input
+                  type="text"
+                  id="email"
+                  className="layout__form-inputs"
+                  placeholder="API"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="buttons-block">
+            <button
+              type="submit"
+              className="buttons-block__single-button buttons-block__single-button--cancel"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="buttons-block__single-button buttons-block__single-button--save"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </article>
+    </main>
   );
 }
 
