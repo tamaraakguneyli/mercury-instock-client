bcf3a05d71f9f044e50267adb9aa75a1ce34e522
tamaraakguneyli <tamara.akguneyli@hotmail.co.uk>
2024-03-26 14:23:35 +0000

    add layout for the edit warehouse form

--- src/components/WarehouseForm/WarehouseForm.js
+++ src/components/WarehouseForm/WarehouseForm.js
@@ -1,7 +1,43 @@
 import React from "react";
 
 function WarehouseForm() {
-  return <div>WarehouseForm</div>;
+  return (
+    <form>
+      <div className="forms">
+        <div className="form">
+          <h1>Warehouse Details</h1>
+          <label>Warehouse Name</label>
+          <input
+            type="text"
+            id="warehouse-name"
+            className=""
+            placeholder="API"
+          />
+          <label>Street Address</label>
+          <input type="text" id="address" className="" placeholder="API" />
+          <label>City</label>
+          <input type="text" id="city" className="" placeholder="API" />
+          <label>Country</label>
+          <input type="text" id="country" className="" placeholder="API" />
+        </div>
+        <div className="form">
+          <h1>Warehouse Details</h1>
+          <label>Contact Name</label>
+          <input type="text" id="contact-name" className="" placeholder="API" />
+          <label>Position</label>
+          <input type="text" id="position" className="" placeholder="API" />
+          <label>Phone Number</label>
+          <input type="text" id="number" className="" placeholder="API" />
+          <label>Email</label>
+          <input type="text" id="email" className="" placeholder="API" />
+        </div>
+      </div>
+      <div>
+        <button type="submit">Cancel</button>
+        <button type="submit">Save</button>
+      </div>
+    </form>
+  );
 }
 
 export default WarehouseForm;
