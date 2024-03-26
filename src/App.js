779fc235766ed908097374b6383c1d9aff1f21e2
mariam1798 <Mariam.alneamah@gmail.com>
2024-03-26 12:20:37 +0000

    add phone and tablet responsiveness styling to page header

--- src/App.js
+++ src/App.js
@@ -11,31 +11,31 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Warehouses />} />
-        <Route path="/warehouses">
-          <Route index element={<Warehouses />} />
-          <Route path="add" element={<AddWarehouse />} />
-          <Route path=":id">
-            <Route index element={<WarehouseDetails />} />
-            <Route path="edit" element={<EditWarehouse />} />
-            <Route path="inventory">
-              <Route index element={<Inventory />} />
-              <Route path="add" element={<AddInventory />} />
-              <Route path=":inventoryId">
-                <Route index element={<InventoryDetails />} />
-                <Route path="edit" element={<EditInventory />} />
-              </Route>
-            </Route>
-          </Route>
-        </Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Warehouses />} />
+				<Route path="/warehouses">
+					<Route index element={<Warehouses />} />
+					<Route path="add" element={<AddWarehouse />} />
+					<Route path=":id">
+						<Route index element={<WarehouseDetails />} />
+						<Route path="edit" element={<EditWarehouse />} />
+						<Route path="inventory">
+							<Route index element={<Inventory />} />
+							<Route path="add" element={<AddInventory />} />
+							<Route path=":inventoryId">
+								<Route index element={<InventoryDetails />} />
+								<Route path="edit" element={<EditInventory />} />
+							</Route>
+						</Route>
+					</Route>
+				</Route>
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,5 +1,19 @@
+import "./MainHeader.scss";
+
 function MainHeader() {
-  return <div>MainHeader</div>;
+	return (
+		<section className="label">
+			<h2 className="label__title">Warehouses</h2>
+			<div className="label__wrapper">
+				<input
+					className="label__search"
+					placeholder="Search..."
+					name="search"
+				></input>
+				<button className="label__button"> + Add New Warehouse</button>
+			</div>
+		</section>
+	);
 }
 
 export default MainHeader;
