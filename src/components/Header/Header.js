cbfabbd82e6c5337e05b5265b40bfffe30cecec9
tamaraakguneyli <tamara.akguneyli@hotmail.co.uk>
2024-03-26 09:59:10 +0000

    add header structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,19 @@
+import React from "react";
+import "../Header/Header.scss";
+import logo from "../../assets/logo/InStock-Logo_2x.png";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <nav className="header__nav">
+        <img src={logo} alt="instock logo" className="header__logo" />
+        <ul className="header__pages">
+          <li className="header__link">Warehouses</li>
+          <li className="header__link">Inventory</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
