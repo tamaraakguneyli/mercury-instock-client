0402a943bb3fdf944dbe424eab3dd5dafe5a0b21
mariam1798 <Mariam.alneamah@gmail.com>
2024-03-27 11:20:59 +0000

    edit prettier spacing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,25 +4,25 @@ import logo from "../../assets/logo/InStock-Logo_2x.png";
 import { Link, NavLink } from "react-router-dom";
 
 function Header() {
-	return (
-		<header className="header">
-			<div className="header__wrapper">
-				<nav className="header__nav">
-					<Link to="/" className="header__img">
-						<img src={logo} alt="instock logo" className="header__logo" />
-					</Link>
-					<div className="header__pages">
-						<NavLink to={"/warehouses"} className="header__link">
-							<p className="header__page">Warehouses</p>
-						</NavLink>
-						<NavLink to={"/Inventory"} className="header__link">
-							<p className="header__page">Inventory</p>
-						</NavLink>
-					</div>
-				</nav>
-			</div>
-		</header>
-	);
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <nav className="header__nav">
+          <Link to="/" className="header__img">
+            <img src={logo} alt="instock logo" className="header__logo" />
+          </Link>
+          <div className="header__pages">
+            <NavLink to={"/warehouses"} className="header__link">
+              <p className="header__page">Warehouses</p>
+            </NavLink>
+            <NavLink to={"/Inventory"} className="header__link">
+              <p className="header__page">Inventory</p>
+            </NavLink>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
