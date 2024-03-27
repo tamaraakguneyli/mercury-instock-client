bd3dc6a03cf6cae71dc26e2bde69a5c42a996878
tnp_167 <tnp_167@outlook.com>
2024-03-27 10:22:22 +0000

    Fix minor styling

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -3,8 +3,31 @@ import "./InventoryList.scss";
 import { Link } from "react-router-dom";
 
 function InventoryList() {
-  const inStock = true;
-  const id = 1;
+  const dataSet = [
+    {
+      id: 1,
+      name: "Television",
+      category: "Electronics",
+      qty: 500,
+      warehouse: "Washington",
+    },
+    {
+      id: 2,
+      name: "Gym Back",
+      category: "Gear",
+      qty: 0,
+      warehouse: "Manhattan",
+    },
+    {
+      id: 3,
+      name: "Hoodie",
+      category: "Apparel",
+      qty: 0,
+      warehouse: "Manhattan",
+    },
+  ];
+
+  const isLastComment = (index) => index === dataSet.length - 1;
   return (
     <div className="list">
       <div className="list__thread">
@@ -17,64 +40,85 @@ function InventoryList() {
           actions
         </h4>
       </div>
-      <div className="list__main list__main--row">
-        <Link
-          to={`/inventory/${id}`}
-          className="list__data list__link list__data--row"
+      {dataSet.map((item, index) => (
+        <div
+          className={`list__main list__main--row ${
+            isLastComment(index)
+              ? "list__main--last"
+              : index === 0
+              ? "list__main--first"
+              : ""
+          }`}
+          key={item.id}
         >
-          Television
-        </Link>
-        <p className="list__data list__category list__data--row">Electronics</p>
-        <div className="list__status--container">
-          <p
-            className={`list__data list__status ${
-              inStock ? "" : "list__status--out"
-            } list__data--row`}
+          <Link
+            to={`/inventory/${item.id}`}
+            className="list__data list__link list__data--row"
           >
-            {`${inStock ? "in stock" : "out of stock"}`}
-          </p>
-        </div>
-        <p className="list__data list__qty list__data--row">500</p>
-        <p className="list__data list__warehouse list__data--row">Manhattan</p>
-        <div className="list__buttons list__buttons--row">
-          <button className="list__delete"></button>
-          <Link to={`/inventory/${id}/edit`} className="list__edit">
-            <button></button>
+            {item.name}
           </Link>
-        </div>
-      </div>
-      <div className="list__main">
-        <div className="list__details">
-          <div className="list__first">
-            <h4 className="list__title">inventory item</h4>
-            <Link to={`/inventory/${id}`} className="list__data list__link">
-              Television
-            </Link>
-            <h4 className="list__title">category</h4>
-            <p className="list__data list__category">Electronics</p>
-          </div>
-          <div className="list__second">
-            <h4 className="list__title">status</h4>
+          <p className="list__data list__category list__data--row">
+            {item.category}
+          </p>
+          <div className="list__status-container">
             <p
               className={`list__data list__status ${
-                inStock ? "" : "list__status--out"
-              }`}
+                item.qty > 0 ? "" : "list__status--out"
+              } list__data--row`}
             >
-              {`${inStock ? "in stock" : "out of stock"}`}
+              {item.qty > 0 ? "in stock" : "out of stock"}
             </p>
-            <h4 className="list__title">qty</h4>
-            <p className="list__data list__qty">500</p>
-            <h4 className="list__title">warehouse</h4>
-            <p className="list__data list__warehouse">Manhattan</p>
+          </div>
+          <p className="list__data list__qty list__data--row">{item.qty}</p>
+          <p className="list__data list__warehouse list__data--row">
+            {item.warehouse}
+          </p>
+          <div className="list__buttons list__buttons--row">
+            <button className="list__delete"></button>
+            <Link to={`/inventory/${item.id}/edit`} className="list__edit">
+              <button></button>
+            </Link>
           </div>
         </div>
-        <div className="list__buttons">
-          <button className="list__delete"></button>
-          <Link to={`/inventory/${id}/edit`} className="list__edit">
-            <button></button>
-          </Link>
+      ))}
+
+      {dataSet.map((item) => (
+        <div className="list__main" key={item.id}>
+          <div className="list__details">
+            <div className="list__first">
+              <h4 className="list__title">inventory item</h4>
+              <Link
+                to={`/inventory/${item.id}`}
+                className="list__data list__link"
+              >
+                {item.name}
+              </Link>
+              <h4 className="list__title">category</h4>
+              <p className="list__data list__category">{item.category}</p>
+            </div>
+            <div className="list__second">
+              <h4 className="list__title">status</h4>
+              <p
+                className={`list__data list__status ${
+                  item.qty > 0 ? "" : "list__status--out"
+                }`}
+              >
+                {`${item.qty > 0 ? "in stock" : "out of stock"}`}
+              </p>
+              <h4 className="list__title">qty</h4>
+              <p className="list__data list__qty">{item.qty}</p>
+              <h4 className="list__title">warehouse</h4>
+              <p className="list__data list__warehouse">{item.warehouse}</p>
+            </div>
+          </div>
+          <div className="list__buttons">
+            <button className="list__delete"></button>
+            <Link to={`/inventory/${item.id}/edit`} className="list__edit">
+              <button></button>
+            </Link>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
