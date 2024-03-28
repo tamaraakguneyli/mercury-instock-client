aee6d7b7b7f5b7cdddae4da18e209c23a52f49cc
tnp_167 <tnp_167@outlook.com>
2024-03-28 12:14:57 +0000

    Add API to get list of all Inventory items from all Warehouses

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -1,32 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function InventoryList() {
-  const dataSet = [
-    {
-      id: 1,
-      name: "Television",
-      category: "Electronics",
-      qty: 500,
-      warehouse: "Washington",
-    },
-    {
-      id: 2,
-      name: "Gym Back",
-      category: "Gear",
-      qty: 0,
-      warehouse: "Manhattan",
-    },
-    {
-      id: 3,
-      name: "Hoodie",
-      category: "Apparel",
-      qty: 0,
-      warehouse: "Manhattan",
-    },
-  ];
-
-  const isLastComment = (index) => index === dataSet.length - 1;
+function InventoryList({ inventories }) {
+  const isLastComment = (index) => index === inventories.length - 1;
   return (
     <div className="page page--margin">
       <div className="list page__content">
@@ -40,7 +16,7 @@ function InventoryList() {
             actions
           </h4>
         </div>
-        {dataSet.map((item, index) => (
+        {inventories.map((item, index) => (
           <div
             className={`list__main list__main--row ${
               isLastComment(index) ? "list__main--last" : ""
@@ -52,7 +28,7 @@ function InventoryList() {
               to={`/inventory/${item.id}`}
               className="list__data list__link list__data--row"
             >
-              {item.name}
+              {item.item_name}
             </Link>
             <p className="list__data list__category list__data--row">
               {item.category}
@@ -60,15 +36,17 @@ function InventoryList() {
             <div className="list__status-container">
               <p
                 className={`list__data list__status ${
-                  item.qty > 0 ? "" : "list__status--out"
+                  item.status === "In Stock" ? "" : "list__status--out"
                 } list__data--row`}
               >
-                {item.qty > 0 ? "in stock" : "out of stock"}
+                {item.status}
               </p>
             </div>
-            <p className="list__data list__qty list__data--row">{item.qty}</p>
+            <p className="list__data list__qty list__data--row">
+              {item.quantity}
+            </p>
             <p className="list__data list__warehouse list__data--row">
-              {item.warehouse}
+              {item.warehouse_name}
             </p>
             <div className="list__buttons list__buttons--row">
               <button className="list__delete"></button>
@@ -79,7 +57,7 @@ function InventoryList() {
           </div>
         ))}
 
-        {dataSet.map((item) => (
+        {inventories.map((item) => (
           <div className="list__main" key={item.id}>
             <div className="list__details">
               <div className="list__first">
@@ -88,7 +66,7 @@ function InventoryList() {
                   to={`/inventory/${item.id}`}
                   className="list__data list__link"
                 >
-                  {item.name}
+                  {item.item_name}
                 </Link>
                 <h4 className="list__title">category</h4>
                 <p className="list__data list__category">{item.category}</p>
@@ -103,9 +81,11 @@ function InventoryList() {
                   {`${item.qty > 0 ? "in stock" : "out of stock"}`}
                 </p>
                 <h4 className="list__title">qty</h4>
-                <p className="list__data list__qty">{item.qty}</p>
+                <p className="list__data list__qty">{item.quantity}</p>
                 <h4 className="list__title">warehouse</h4>
-                <p className="list__data list__warehouse">{item.warehouse}</p>
+                <p className="list__data list__warehouse">
+                  {item.warehouse_name}
+                </p>
               </div>
             </div>
             <div className="list__buttons">
