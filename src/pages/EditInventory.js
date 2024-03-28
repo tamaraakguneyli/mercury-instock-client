ad5ece8ead50e42e0c957b378f9b4ef8b3f1f286
tnp_167 <tnp_167@outlook.com>
2024-03-28 17:30:06 +0000

    Fetch selected inventory from API

--- src/pages/EditInventory.js
+++ src/pages/EditInventory.js
@@ -1,7 +1,32 @@
 import PageHeader from "../components/PageHeader/PageHeader";
 import InventoryForm from "../components/InventoryForm/InventoryForm";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import apiConfig from "../apiConfig.json";
 
 function EditInventory() {
+  const { inventoryId } = useParams();
+  const [inventoryItemDetails, setInventoryDetails] = useState(null);
+
+  useEffect(() => {
+    const getInventoryItemDetails = async () => {
+      try {
+        const { data } = await axios.get(
+          `${apiConfig.baseUrl}/inventory/${inventoryId}${apiConfig.urlParam}`
+        );
+        setInventoryDetails(data);
+      } catch (error) {
+        console.log("Error while fetching inventory item:", error);
+      }
+    };
+    getInventoryItemDetails();
+  }, [inventoryId]);
+
+  if (!inventoryItemDetails) {
+    return <p>Loading...</p>;
+  }
+
   const headerConfig = {
     backButton: {
       show: true,
@@ -18,7 +43,7 @@ function EditInventory() {
   return (
     <>
       <PageHeader title="Edit Inventory Item" config={headerConfig} />
-      <InventoryForm action="edit" apiData={{}} />
+      <InventoryForm action="edit" apiData={{ inventoryItemDetails }} />
     </>
   );
 }
