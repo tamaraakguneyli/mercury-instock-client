95fe2b6fb5bb224b563c47c1087ee5e243e8cddc
tnp_167 <tnp_167@outlook.com>
2024-03-29 09:46:42 +0000

    Restyle the inventory list for tablet device

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function InventoryList({ inventories }) {
   const isLastComment = (index) => index === inventories.length - 1;
   return (
-    <div className="page page--margin">
+    /*<div className="page page--margin">
       <div className="list page__content">
         <div className="list__thread">
           <h4 className="list__title list__title--top">inventory item</h4>
@@ -55,11 +55,28 @@ function InventoryList({ inventories }) {
               </Link>
             </div>
           </div>
-        ))}
+        ))}*/
 
-        {inventories.map((item) => (
-          <div className="list__main" key={item.id}>
-            <div className="list__details">
+    <div className="page page--margin">
+      <div className="list page__content">
+        <div className="list__thread">
+          <h4 className="list__title list__title--top">inventory item</h4>
+          <h4 className="list__title list__title--top">category</h4>
+          <h4 className="list__title list__title--top">status</h4>
+          <h4 className="list__title list__title--top">quantity</h4>
+          <h4 className="list__title list__title--top">warehouse</h4>
+          <h4 className="list__title list__title--top list__title--actions">
+            actions
+          </h4>
+        </div>
+        {inventories.map((item, index) => (
+          <div
+            className={`list__main list__main--row ${
+              isLastComment(index) ? "list__main--last" : ""
+            }`}
+            key={item.id}
+          >
+            <div className="list__details list__details--row">
               <div className="list__first">
                 <h4 className="list__title">inventory item</h4>
                 <Link
@@ -69,30 +86,37 @@ function InventoryList({ inventories }) {
                   {item.item_name}
                 </Link>
                 <h4 className="list__title">category</h4>
-                <p className="list__data list__category">{item.category}</p>
+                <p className="list__data list__data--category list__category">
+                  {item.category}
+                </p>
               </div>
               <div className="list__second">
                 <h4 className="list__title">status</h4>
-                <p
-                  className={`list__data list__status ${
-                    item.qty > 0 ? "" : "list__status--out"
-                  }`}
-                >
-                  {`${item.qty > 0 ? "in stock" : "out of stock"}`}
-                </p>
+                <div className="list__status-container">
+                  <p
+                    className={`list__data list__status ${
+                      item.status === "In Stock" ? "" : "list__status--out"
+                    }`}
+                  >
+                    {`${item.status}`}
+                  </p>
+                </div>
                 <h4 className="list__title">qty</h4>
-                <p className="list__data list__qty">{item.quantity}</p>
+                <p className="list__data list__data--qty list__qty">
+                  {item.quantity}
+                </p>
                 <h4 className="list__title">warehouse</h4>
-                <p className="list__data list__warehouse">
+                <p className="list__data list__data--warehouse list__warehouse">
                   {item.warehouse_name}
                 </p>
               </div>
             </div>
             <div className="list__buttons">
               <button className="list__delete"></button>
-              <Link to={`/inventory/${item.id}/edit`} className="list__edit">
-                <button></button>
-              </Link>
+              <Link
+                to={`/inventory/${item.id}/edit`}
+                className="list__edit"
+              ></Link>
             </div>
           </div>
         ))}
